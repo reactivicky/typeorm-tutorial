589d94a028b480aa980195e75d7a40cf80952164
Vignesh <reactivicky@gmail.com>
2024-08-15 18:57:17 +0530

    Setup typeorm with postgres

--- src/app.ts
+++ src/app.ts
@@ -4,16 +4,15 @@ import express, { Request, Response } from 'express'
 import dataSource from "./dataSource";
 const app = express();
 
+dataSource.initialize().then(() => {
+  console.log("DataSource connected.")
+}).catch(err => console.log(err))
+
 app.get("/", (req: Request, res: Response) => {
   res.send('hello world')
 })
 
 const PORT = 4000
 app.listen(PORT, async () => {
-  try {
-    await dataSource.initialize()
-    console.log(`DataSource connected. Server is listening on ${PORT}`)
-  } catch (error) {
-    console.log(error)
-  }
+  console.log(`Server is listening on ${PORT}`)
 })
